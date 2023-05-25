b06bfab8fe0217545468876aba95ecaec766b453
maftuhlutfi <playmakernyomlo@gmail.com>
2023-05-25 14:17:20 +0700

    add gtag id

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=GTM-TGZ2S4Q"
+          strategy="afterInteractive"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TGZ2S4Q"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
       </body>
     </Html>
-  )
+  );
 }
